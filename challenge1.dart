b5817285dd7eca5da17ffd27ae2f3e067639759e
athouba99 <athooba99@gmail.com>
2022-11-08 19:01:14 +0300

    dart task3 challenge1 'still'

--- challenge1.dart
+++ challenge1.dart
@@ -3,6 +3,9 @@
  * Create a function called `printName`
  * - that prints your name on the screen.
  */
+void name() {
+  print("Athoub BUtaiban");
+}
 
 /**
  * Task 2:
@@ -11,6 +14,11 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+void printAge(int birth) {
+  int age;
+  age = 2022 - birth;
+  print(age);
+}
 
 /**
  * Task 3:
@@ -22,6 +30,32 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
+void printHello(String name, String language) {
+  // //String en, es, fr, tr;
+  // en = "Hello NAME";
+  // if (language == "en") {
+  //   print("Hello $name");
+  // } else if (language == es) {
+  //   es = "Hola NAME";
+  //   print("Hola $name");
+  // } else if (language == fr) {
+  //   fr = "Bonjour NAME";
+  //   print("Bonjour $name");
+  // } else {
+  //   tr = "Merhaba NAME";
+  //   print("Merhaba $name");
+  // }
+
+  if (language == "en") {
+    print("hello $name");
+  } else if (language == "es") {
+    print("hola $name");
+  } else if (language == "fr") {
+    print("bonjour $name");
+  } else {
+    print("Merhaba $name");
+  }
+}
 
 /**
  * Task 4:
@@ -29,3 +63,13 @@
  * - that takes two numbers as parameters,
  * - and prints out the bigger number
  */
+
+void printMax(int a, int b) {
+  print(int);
+}
+
+void main() {
+  name(); // task1
+  printAge(1999); // task2
+  printHello("hello", "en"); // task3
+}
